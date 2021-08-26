fa4eb9b7bb730a12e7e3d072ce96b5a8c7225ca6
K.Idel <k.idel@htsts.ru>
2021-08-26 17:19:20 +0300

    builder added

---
+++ Patterns/builder.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+#include <memory>
+
+class Product {
+
+public:
+
+	Product() = default;	
+	void sayHi() {
+
+		std::cout << "Hi from Product" << std::endl;
+	}
+};
+
+
+class Builder{
+
+public:
+    
+    virtual ~Builder(){}
+    virtual void ProducePartA() const = 0;
+    virtual void ProducePartB() const = 0;
+    virtual void ProducePartC() const = 0;
+};
+
+
+class ConcreteBuilder : public Builder{
+
+private:
+    std::unique_ptr<Product> product{};
+
+public:
+    ConcreteBuilder() = default;
+
+    void ProducePartA()const override{
+        std::cout << "PartA" << std::endl;
+    }
+
+    void ProducePartB()const override{
+        std::cout << "PartB" << std::endl;
+    }
+
+    void ProducePartC()const override{
+        std::cout << "PartC" << std::endl;
+    }
+
+    std::unique_ptr<Product>&& GetProduct() {
+
+        return std::move(this->product);
+    }
+};
+
+
+class Director {
+
+private:
+	const Builder& builder{};
+
+public:
+	Director(const Builder& builder) : builder(builder) {};
+	
+	void buildConfigBase() {
+
+		std::cout << "buildConfigBase" << std::endl;
+		builder.ProducePartA();
+	}
+
+	void buildConfigWide() {
+		
+		std::cout << "buildConfigWide" << std::endl;
+		builder.ProducePartA();
+		builder.ProducePartB();
+	}
+
+	void buildConfigAll() {
+
+        std::cout << "buildConfigAll" << std::endl;
+		builder.ProducePartA();
+		builder.ProducePartB();
+		builder.ProducePartC();
+	}
+};
+
+int main() {
+
+	ConcreteBuilder cb;
+	Director dir{cb};
+
+	dir.buildConfigBase();
+	dir.buildConfigAll();
+}
+
+
+
---
+++ Patterns/interface_realization.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <memory>
+
+/***********************************************************/
+class iEmploee {
+
+public:
+	virtual ~iEmploee(){};
+	virtual void doWork() const = 0;
+};
+
+
+/***********************************************************/
+class Designer : public iEmploee {
+
+public:
+
+	void doWork() const override {
+
+		std::cout << "I am making cool design, sir!" << std::endl;
+	}
+};
+
+class Progger : public iEmploee {
+
+public:
+
+	void doWork() const override {
+
+		std::cout << "I am making producing a lot of bugs. Sorry..." << std::endl;
+	}
+};
+
+
+/***********************************************************/
+using iEploee_uptr_t = std::unique_ptr<iEmploee>;
+
+int main () {
+
+	iEploee_uptr_t designer{new Designer};
+	iEploee_uptr_t progger{new Progger};
+
+	designer->doWork();
+	progger->doWork();
+}
