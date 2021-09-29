ba5c7ac99f8469908e7e77065f2ae21349a2dc23
K.Idel <k.idel@htsts.ru>
2021-09-29 22:25:51 +0300

    builder replaced

--- Patterns/builder.cpp
+++ Patterns/builder.cpp
@@ -1,95 +1,71 @@
+#include <vector>
 #include <iostream>
 #include <memory>
 
-/* Not used yet */
-class Product {
+class foo {
+private:
+
+    int m_prop1;
+    bool m_prop2;
+    bool m_prop3;
+    std::vector<int> m_prop4;
 
 public:
+    class builder;
 
-	Product() = default;	
-	void sayHi() {
+    foo(int prop1, bool prop2, bool prop3, std::vector<int> prop4) : 
+    m_prop1(prop1), m_prop2(prop2), m_prop3(prop3), m_prop4(prop4) {}
 
-		std::cout << "Hi from Product" << std::endl;
-	}
+    void showParams() {
+
+        std::cout << "====================================" << std::endl;
+        std::cout << "m_prop1 -> " << m_prop1 << std::endl;
+        std::cout << "m_prop2 -> " << std::boolalpha << m_prop2 << std::endl;
+        std::cout << "m_prop3 -> " << std::boolalpha << m_prop3 << std::endl;
+
+        for(const auto& i : m_prop4) {
+
+            std::cout << "p_prop4 -> " << i << std::endl;
+        }
+    }
 };
 
-
-class Builder {
+class foo::builder {
 
 public:
-    
-    virtual ~Builder(){}
-    virtual void ProducePartA() const = 0;
-    virtual void ProducePartB() const = 0;
-    virtual void ProducePartC() const = 0;
-};
+    builder& set_prop1(int value) {m_prop1 = value; return *this;};
+    builder& set_prop2(bool value) {m_prop2 = value; return *this;};
+    builder& set_prop3(bool value) {m_prop3 = value; return *this;};
+    builder& set_prop4(std::vector<int> value) {m_prop4 = value; return *this;};
 
-
-class ConcreteBuilder : public Builder {
+    foo build() const {
+        
+        return std::move(foo{m_prop1, m_prop2, m_prop3, m_prop4});
+    }
 
 private:
-    /* Not used yet */
-    std::unique_ptr<Product> product{};
 
-public:
-    ConcreteBuilder() = default;
-
-    void ProducePartA() const override {
-        std::cout << "PartA" << std::endl;
-    }
-
-    void ProducePartB() const override {
-        std::cout << "PartB" << std::endl;
-    }
-
-    void ProducePartC() const override {
-        std::cout << "PartC" << std::endl;
-    }
-
-    /* Not used yet */
-    std::unique_ptr<Product>&& GetProduct() {
-
-        return std::move(this->product);
-    }
+    int m_prop1{0};
+    bool m_prop2{false};
+    bool m_prop3{false};
+    std::vector<int> m_prop4{5, 2, 42};
 };
 
+foo::builder makeBuilder() {
 
-class Director {
-
-private:
-	const Builder& builder{};
-
-public:
-	Director(const Builder& builder) : builder(builder) {};
-	
-	void buildConfigBase() {
-
-		std::cout << "buildConfigBase" << std::endl;
-		builder.ProducePartA();
-	}
-
-	void buildConfigWide() {
-		
-		std::cout << "buildConfigWide" << std::endl;
-		builder.ProducePartA();
-		builder.ProducePartB();
-	}
-
-	void buildConfigAll() {
-
-        std::cout << "buildConfigAll" << std::endl;
-		builder.ProducePartA();
-		builder.ProducePartB();
-		builder.ProducePartC();
-	}
-};
-
+  return foo::builder{};
+}
 
 int main() {
 
-	ConcreteBuilder cb;
-	Director dir{cb};
+  foo obj_1 = makeBuilder()
+          .set_prop1(5)
+          .set_prop3(true)
+          .set_prop4({1,2,3})
+          .build();
+  
+  obj_1.showParams();
 
-	dir.buildConfigBase();
-	dir.buildConfigAll();
+  foo obj_2 = makeBuilder().build();
+  obj_2.showParams();
 }
