4538bc2f4c29b482e052c5fdc98ca44183d78c2d
K.Idel <k.idel@htsts.ru>
2021-11-02 20:54:37 +0300

    Добавлен генератор UUID

---
+++ Network/Chat_with_UNIX_sockets/CUUIDGenerator/CUUIDGenerator.cpp
@@ -0,0 +1,157 @@
+#include <iostream>
+#include <uuid/uuid.h>
+#include <random>
+#include <array>
+#include <algorithm>
+#include <openssl/md5.h>
+#include "CUUIDGenerator.h"
+
+const std::string& uuid::CUUIDGenerator::getTemplateString() {
+
+    const static std::string sAllowed{"0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"};
+    return sAllowed;
+}
+
+std::string uuid::CUUIDGenerator::getNewUUID(uint64_t uuidType, size_t nRequiredLen) {
+    
+    std::string sUUID;
+    if (uuidType & generator_type::SYSTEM_GENERATOR)
+    {
+        sUUID = getRandomSystemString();
+
+        COUT << "UUID SYSTEM_GENERATOR:\t" << sUUID.c_str()  << " len = " << sUUID.length() << std::endl;
+
+    }
+
+    if (uuidType & generator_type::RANDOM_GENERATOR)
+    {
+        sUUID += getRandomString(nRequiredLen);
+        COUT << "UUID RANDOM_GENERATOR:\t" << sUUID.c_str()  << " len = " << sUUID.length() << std::endl;
+    }
+
+    if (uuidType & generator_type::TIME_GENERATOR)
+    {
+        sUUID += getCurrentTimeString();
+
+        COUT << "UUID TIME_GENERATOR:\t" << sUUID.c_str()  << " len = " << sUUID.length() << std::endl;
+
+    }
+
+
+    if (nRequiredLen > DEFAULT_UUID_STRING_LEN)
+    {
+        if (nRequiredLen != sUUID.length())
+        {
+            if (nRequiredLen > sUUID.length())
+            {
+                std::string sTail = getRandomString(nRequiredLen - sUUID.length());
+                sUUID += sTail;
+            } else {
+                std::random_shuffle(sUUID.begin(), sUUID.end());
+                sUUID = sUUID.substr(0, nRequiredLen);
+            }
+        }
+    }
+    else
+        sUUID = getMD5(sUUID);
+
+    COUT << "Final UUID:\t\t" << sUUID.c_str()  << " len = " << sUUID.length() << " nRequiredLen = " << nRequiredLen << std::endl;
+
+    return sUUID;
+}
+
+std::string uuid::CUUIDGenerator::getMD5(const std::string &strSource)
+{
+
+    std::array<unsigned char, MD5_DIGEST_LENGTH> sResultMD5;
+    MD5(reinterpret_cast<const unsigned char *>(strSource.data()), strSource.size(), sResultMD5.data());
+
+
+    std::string sResult;
+    sResult.reserve(2 * MD5_DIGEST_LENGTH);
+    for (unsigned char hashChar: sResultMD5)
+    {
+        sResult += "0123456789ABCDEF"[hashChar / 16];
+        sResult += "0123456789ABCDEF"[hashChar % 16];
+    }
+
+    return sResult;
+}
+
+std::string uuid::CUUIDGenerator::getRandomSystemString()
+{
+    uuid_t uuidObj;
+    uuid_generate(uuidObj);
+
+    std::array<char, SIZE_OF_SYSTEM_UUID> arSystemUUID;
+    uuid_unparse(uuidObj, &arSystemUUID.front());
+
+    std::string systemUUID(std::begin(arSystemUUID), std::end(arSystemUUID));
+
+    systemUUID.erase(std::remove(systemUUID.begin(), systemUUID.end(),L'-'),
+                     systemUUID.end());
+
+    return systemUUID;
+}
+
+std::string uuid::CUUIDGenerator::getCurrentTimeString()
+{
+    time_stamp_microseconds ts = std::chrono::time_point_cast<std::chrono::microseconds>(std::chrono::system_clock::now());
+    std::string sCurTime = std::to_string(ts.time_since_epoch().count());
+
+    return sCurTime;
+}
+
+std::string uuid::CUUIDGenerator::getRandomString(size_t nLen)
+{
+    thread_local static std::mt19937 rndEng{std::random_device{}()};
+    thread_local static std::uniform_int_distribution<std::string::size_type> rndIndex(0, uuid::CUUIDGenerator::getTemplateString().length() - 1);
+
+    std::string sRandom;
+
+    sRandom.reserve(nLen);
+
+    while(nLen--)
+        sRandom += getTemplateString()[rndIndex(rndEng)];
+
+    return sRandom;
+}
+
+
+std::string uuid::CUUIDGenerator::getRandomStringFast(size_t nLen)
+{
+
+    std::string sRandom;
+
+    sRandom.reserve(nLen);
+
+    while(nLen--)
+        sRandom += getTemplateString()[uuid::CUUIDGenerator::getBoundedRandValue(getTemplateString().length() - 1)];
+
+    return sRandom;
+}
+
+// Экспериментальная быстрая функция генерации случайных чисел
+// в заданном диапазоне
+// см. http://www.pcg-random.org/posts/bounded-rands.html
+uint32_t uuid::CUUIDGenerator::getBoundedRandValue(uint32_t range)
+{
+    thread_local static std::mt19937 rng{std::random_device{}()};
+    uint32_t x = rng();
+    uint64_t m = uint64_t(x) * uint64_t(range);
+    uint32_t l = uint32_t(m);
+    if (l < range) {
+        uint32_t t = -range;
+        if (t >= range) {
+            t -= range;
+            if (t >= range)
+                t %= range;
+        }
+        while (l < t) {
+            x = rng();
+            m = uint64_t(x) * uint64_t(range);
+            l = uint32_t(m);
+        }
+    }
+    return m >> 32;
+}
---
+++ Network/Chat_with_UNIX_sockets/Include/CUUIDGenerator.h
@@ -0,0 +1,49 @@
+#ifndef UUIDMANAGER_CUUIDGENERATOR_H
+#define UUIDMANAGER_CUUIDGENERATOR_H
+
+#include <string>
+#include <chrono>
+#include <openssl/md5.h>
+
+using time_stamp_microseconds = std::chrono::time_point<std::chrono::system_clock, std::chrono::microseconds>;
+
+const size_t DEFAULT_RANDOM_STRING_LEN = 2 * MD5_DIGEST_LENGTH;
+const size_t DEFAULT_UUID_STRING_LEN = 2 * MD5_DIGEST_LENGTH;
+
+namespace uuid {
+    
+	const int SIZE_OF_SYSTEM_UUID = 36;
+    namespace generator_type{
+        
+        const uint64_t SYSTEM_GENERATOR = 1;
+        const uint64_t RANDOM_GENERATOR = 2;
+        const uint64_t TIME_GENERATOR = 8;
+        const uint64_t COMPLETE_GENERATOR = SYSTEM_GENERATOR | RANDOM_GENERATOR | TIME_GENERATOR;
+    }
+
+    class CUUIDGenerator {
+    public:
+
+        static std::string getNewUUID(uint64_t uuidType = generator_type::COMPLETE_GENERATOR, 
+            size_t nRequiredLen = DEFAULT_UUID_STRING_LEN);
+        
+        static std::string getRandomString(size_t nLen = DEFAULT_RANDOM_STRING_LEN);
+        static std::string getRandomStringFast(size_t nLen = DEFAULT_RANDOM_STRING_LEN);
+        static std::string getCurrentTimeString();
+        static std::string getRandomSystemString();
+
+    private:
+        static const std::string& getTemplateString();
+        static std::string getMD5(const std::string &strSource);
+        static uint32_t getBoundedRandValue(uint32_t range);
+    };
+}
+
+#ifdef UUID_LIB_DEBUG
+#define COUT std::cout
+#else
+#define COUT if(0) std::cout
+#endif
+
+#endif //UUIDMANAGER_CUUIDGENERATOR_H
+
