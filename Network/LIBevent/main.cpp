18980efd13be256eda23ddbce1bfc1d71d037435
K.Idel <k.idel@htsts.ru>
2021-12-19 23:29:18 +0300

    модифицировал пример на libevent

--- Network/LIBevent/main.cpp
+++ Network/LIBevent/main.cpp
@@ -22,6 +22,43 @@ int main()
     auto *OutBuf = evhttp_request_get_output_buffer(req);
     if (!OutBuf)
       return;
+
+    const char *cmdtype;
+
+    switch (evhttp_request_get_command(req)) {
+      case EVHTTP_REQ_GET: cmdtype = "GET"; break;
+      case EVHTTP_REQ_POST: cmdtype = "POST"; break;
+      case EVHTTP_REQ_HEAD: cmdtype = "HEAD"; break;
+      case EVHTTP_REQ_PUT: cmdtype = "PUT"; break;
+      case EVHTTP_REQ_DELETE: cmdtype = "DELETE"; break;
+      case EVHTTP_REQ_OPTIONS: cmdtype = "OPTIONS"; break;
+      case EVHTTP_REQ_TRACE: cmdtype = "TRACE"; break;
+      case EVHTTP_REQ_CONNECT: cmdtype = "CONNECT"; break;
+      case EVHTTP_REQ_PATCH: cmdtype = "PATCH"; break;
+      default: cmdtype = "unknown"; break;
+    }
+
+    const char* URI = evhttp_request_get_uri(req);
+
+    struct evkeyvalq* headers = evhttp_request_get_input_headers(req);
+    struct evkeyval *header;
+
+    printf("Request type: %s\n", cmdtype);
+    printf("Request URI: %s\n", URI);
+    printf("Request headers: \n");
+
+    for (header = headers->tqh_first; header; header = header->next.tqe_next) {
+      printf("\t%s: %s\n", header->key, header->value);
+    }
+
+    struct evbuffer *buf = evhttp_request_get_input_buffer(req);
+
+    char data[0xFFF];
+
+    evbuffer_copyout(buf, data, evbuffer_get_length(buf));
+
+    printf("Request body: %s\n", data);
+
     evbuffer_add_printf(OutBuf, "<html><body><center><h1>Hello world!</h1></center></body></html>");
     evhttp_send_reply(req, HTTP_OK, "", OutBuf);
   };
