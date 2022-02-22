e211c68537315de18a085bf5425286d657710024
K.Idel <k.idel@htsts.ru>
2022-02-22 14:25:01 +0300

    fix

---
+++ ZMQ/suber.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <zmq.hpp>
+#include <memory>
+
+#include "zhelpers.h"
+
+int main (int argc, char *argv[]) {
+
+    zmq::context_t context (1);
+    std::shared_ptr<zmq::socket_t> receiver{};
+
+    try {
+        
+        const std::string subAddr{std::string("tcp://localhost:5555")};
+        
+        receiver = std::make_shared<zmq::socket_t>(zmq::socket_t{context, ZMQ_SUB});
+        receiver->setsockopt(ZMQ_SUBSCRIBE, "", 0);
+        receiver->connect(subAddr);
+    }
+    catch(const zmq::error_t ze) {
+
+        std::cerr << "Не удалось инициировать сокет (connect). Причина: " << ze.what() << std::endl;
+        return -1;
+    }
+
+	while (true) {
+
+        auto zmqMessage = s_recv(*receiver);
+
+        if(!zmqMessage.empty()) {
+
+            std::cout << zmqMessage << std::endl;
+        }
+    }
+}
