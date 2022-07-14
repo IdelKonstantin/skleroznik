73409345dc9a028bd6e2fa433edf7c2109c14282
K.Idel <k.idel@htsts.ru>
2022-07-14 12:56:46 +0300

    fix

--- ZMQ/publisher.cpp
+++ ZMQ/publisher.cpp
@@ -6,22 +6,22 @@
 int main (int argc, char *argv[]) {
 
 	zmq::context_t context (1);
-    zmq::socket_t publisher (context, ZMQ_PUB);
+	zmq::socket_t publisher (context, ZMQ_PUB);
 
-    try {
+	try {
 
 		const std::string pubAddr{std::string("tcp://*:5555")};
 		publisher.bind(pubAddr);
 	}
 	catch(const zmq::error_t ze) {
 
-        std::cerr << "Не удалось инициировать сокеты (bind). Причина: " << ze.what() << std::endl;
-        return -1;
-    }
+		std::cerr << "Не удалось инициировать сокеты (bind). Причина: " << ze.what() << std::endl;
+		return -1;
+	}
 
 	while (true) {
 
 		std::string zmqMessage = "Hello world!";
-        s_send(publisher, zmqMessage, ZMQ_DONTWAIT);
-    }
+		s_send(publisher, zmqMessage, ZMQ_DONTWAIT);
+	}
 }
--- ZMQ/suber.cpp
+++ ZMQ/suber.cpp
@@ -23,7 +23,7 @@ int main (int argc, char *argv[]) {
         return -1;
     }
 
-	while (true) {
+    while (true) {
 
         auto zmqMessage = s_recv(*receiver);
 
