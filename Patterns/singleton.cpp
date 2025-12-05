0052b40fb8e04150092ea206a248b7a027d70905
IdelKonstantin <49098634+IdelKonstantin@users.noreply.github.com>
2025-12-05 12:06:43 +0300

    Update singleton.cpp

--- Patterns/singleton.cpp
+++ Patterns/singleton.cpp
@@ -1,30 +1,43 @@
 #include <iostream>
 
+namespace device {
+
+    template<typename T>
+    class Singleton {
+        protected:
+        
+            Singleton() = default;
+            ~Singleton() = default;
+    
+        public:
+        
+            Singleton(const Singleton&) = delete;
+            Singleton& operator=(const Singleton&) = delete;
+    
+            static T& getInstance() {
+                
+                static T instance;
+                return instance;
+            }
+    };
+
+} // namespace device
+
+/////////////////////////////////////////////////////////////////////////
+
 class foo {
-    
+  
     public:
-    
-        static foo& getInstance() {
-            
-            static foo f;
-            return f;
+        foo() = default;
+        void print() {
+            std::cout << "Foooooo!!!!" << std::endl;
         }
-        
-        void sayHello() {
-            
-            std::cout << "Hello World" << std::endl;
-        }
-        
-        ~foo() {}
-    
-    private:
-    
-        foo() {}
 };
 
+/////////////////////////////////////////////////////////////////////////
 
 int main()
 {
-    foo::getInstance().sayHello();
+    device::Singleton<foo>::getInstance().print();
     return 0;
 }
